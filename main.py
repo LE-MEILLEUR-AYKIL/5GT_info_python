f2dd7df90e3733d719a415617bf438567759d24d
LE-MEILLEUR-AYKIL <onedlaw61@gmail.com>
2025-01-31 12:00:45 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+
+nombre1 = input("Que vaut le premier nombre ? ")
+nombre2 = input("Que vaut le deuxieme nombre ? ")
+
+
+print("La somme de",nombre1,"et",nombre2,"vaut : ")
+print(nombre1+nombre2)
