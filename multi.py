f0e59bda8c0779993157c1baff96f50bd1a10a6a
LE-MEILLEUR-AYKIL <onedlaw61@gmail.com>
2025-03-28 12:12:46 +0100

    Add files via upload

---
+++ multi.py
@@ -0,0 +1,6 @@
+nombre=input("Bonjour, bienvenue dans ce programme de multiplication. Tu veux connaitre quelle table, DEPECHE TOI DE CHOISIR?")
+
+nombre=int(nombre)
+
+for i in range(1,56):
+  print(i,"x",nombre,"=",i*nombre)
